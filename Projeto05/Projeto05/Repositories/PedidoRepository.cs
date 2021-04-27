f06f9d10456f454f3bb40df727bc9891000a16da
Itamarrr <itamar.oliveira@unigranrio.br>
2021-04-26 21:03:45 -0300

    crud criado

--- Projeto05/Projeto05/Repositories/PedidoRepository.cs
+++ Projeto05/Projeto05/Repositories/PedidoRepository.cs
@@ -1,36 +1,98 @@
+
+using Dapper;
 using Projeto05.Entities;
 using Projeto05.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace Projeto05.Repositories
 {
- public class PedidoRepository : IPedidoRepository
+  public class PedidoRepository : IPedidoRepository
   {
+    private string connectionstring;
+
+    public PedidoRepository(string connectionstring)
+    {
+      this.connectionstring = connectionstring;
+    }
+
     public void Inserir(Pedido obj)
     {
-      throw new NotImplementedException();
+      var query = @"
+                        INSERT INTO PEDIDO(IDPEDIDO, IDCLIENTE, DATAPEDIDO)
+                        VALUES(@IdPedido, @IdCliente, @DataPedido)
+                    ";
+
+      using (var connection = new SqlConnection(connectionstring))
+      {
+        connection
+            .Execute(query, obj);
+      }
     }
 
     public void Alterar(Pedido obj)
     {
-      throw new NotImplementedException();
+      var query = @"
+                        UPDATE PEDIDO
+                        SET
+                            IDCLIENTE = @IdCliente,
+                            DATAPEDIDO = @DataPedido
+                        WHERE
+                            IDPEDIDO = @IdPedido
+                    ";
+
+      using (var connection = new SqlConnection(connectionstring))
+      {
+        connection
+            .Execute(query, obj);
+      }
     }
 
     public void Excluir(Pedido obj)
     {
-      throw new NotImplementedException();
+      var query = @"
+                        DELETE FROM PEDIDO
+                        WHERE IDPEDIDO = @IdPedido
+                    ";
+
+      using (var connection = new SqlConnection(connectionstring))
+      {
+        connection
+            .Execute(query, obj);
+      }
     }
 
     public List<Pedido> ObterTodos()
     {
-      throw new NotImplementedException();
+      var query = @"
+                        SELECT * FROM PEDIDO
+                        ORDER BY DATAPEDIDO
+                    ";
+
+      using (var connection = new SqlConnection(connectionstring))
+      {
+        return connection
+                .Query<Pedido>(query)
+                .ToList();
+      }
     }
 
     public Pedido ObterPorId(Guid id)
     {
-      throw new NotImplementedException();
+      var query = @"
+                        SELECT * FROM PEDIDO
+                        WHERE IDPEDIDO = @id
+                    ";
+
+      using (var connection = new SqlConnection(connectionstring))
+      {
+        return connection
+                .Query<Pedido>(query, new { id })
+                .FirstOrDefault();
+      }
     }
   }
 }
